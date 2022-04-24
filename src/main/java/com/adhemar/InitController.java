3e8ccee3759dfe051ed8e7d27e81d30a032ac7ea
adhes14 <adhemarduran@gmail.com>
2022-04-23 21:23:21 -0400

    Display index page

--- src/main/java/com/adhemar/InitController.java
+++ src/main/java/com/adhemar/InitController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class InitController {
     @GetMapping("/")
     public String init() {
-        return "Hello world with Spring Boot";
+        return "index";
     }
 }
