3f981be00ffd86ab8f8b39af7113a2d97215c673
adhes14 <adhemarduran@gmail.com>
2022-04-23 21:53:42 -0400

    send a message from the Controller

--- src/main/java/com/adhemar/InitController.java
+++ src/main/java/com/adhemar/InitController.java
@@ -1,12 +1,15 @@
 package com.adhemar;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class InitController {
     @GetMapping("/")
-    public String init() {
+    public String init(Model model) {
+        String message = "Hello world with Thymeleaf";
+        model.addAttribute("message", message);
         return "index";
     }
 }
