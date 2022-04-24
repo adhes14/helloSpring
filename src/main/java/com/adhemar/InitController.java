84a9db18cbb72a9a0731d535448645fdcfb627f7
adhes14 <adhemarduran@gmail.com>
2022-04-24 17:52:17 -0400

    Render a list

--- src/main/java/com/adhemar/InitController.java
+++ src/main/java/com/adhemar/InitController.java
@@ -1,6 +1,8 @@
 package com.adhemar;
 
 import com.adhemar.domains.People;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,14 +16,25 @@ public class InitController {
     @GetMapping("/")
     public String init(Model model) {
         String message = "Hello world with Thymeleaf";
+        
         People person = new People();
         person.setFirstName("Juan");
         person.setLastName("Perez");
         person.setEmail("jperez@mail.com");
         person.setPhone("852147963");
+        
+        People person2 = new People();
+        person2.setFirstName("Karla");
+        person2.setLastName("Guzman");
+        person2.setEmail("kguzman@mail.com");
+        person2.setPhone("852369741");
+        
+        List<People> people = Arrays.asList(person, person2);
+        
         model.addAttribute("message", message);
         model.addAttribute("greetings", greetings);
-        model.addAttribute("person", person);
+//        model.addAttribute("person", person);
+        model.addAttribute("people", people);
         return "index";
     }
 }
