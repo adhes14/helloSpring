73d35e7ca3386e26e42ef3368264d6cb110a2628
adhes14 <adhemarduran@gmail.com>
2022-04-23 22:03:34 -0400

    display a message from application.properties

--- src/main/java/com/adhemar/InitController.java
+++ src/main/java/com/adhemar/InitController.java
@@ -1,15 +1,20 @@
 package com.adhemar;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class InitController {
+    @Value("${index.greetings}")
+    private String greetings;
+
     @GetMapping("/")
     public String init(Model model) {
         String message = "Hello world with Thymeleaf";
         model.addAttribute("message", message);
+        model.addAttribute("greetings", greetings);
         return "index";
     }
 }
